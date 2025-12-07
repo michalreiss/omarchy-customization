f323e76b721850e0126b1c1526e8eb02e04173c2
Michał Reiss <michalreiss@hotmail.com>
2025-12-08 00:12:26 +0100

    feat: added other aliases

--- install-shell-aliases.sh
+++ install-shell-aliases.sh
@@ -17,6 +17,10 @@ alias gs="git status"
 alias gss="git status --short"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
 
+# Other
+alias vim="nvim"
+alias cat="bat"
+
 EOF
 
 echo "Aliases added to ~/.bashrc"
